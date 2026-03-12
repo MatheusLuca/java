985cd7120b2a5a751f9e6db09668cc16572afcfe
MatheusLuca <luca_matheus@live.com>
2026-03-11 22:49:29 -0300

    Java Apostila

---
+++ Apostila003/src/Teste.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import
+public class Teste {
+    public static void main(String[] args) {
+
+        System.out.print("Boa noite!!");
+
+        //Declarar variaveis
+        boolean ativo;
+
+        //Atribuir valor para a variavel
+        ativo = true; // ou false
+        System.out.println(ativo);
+
+        // Declarar variaveis e atribuir o valor
+        char sexo = 'F';
+        System.out.println(sexo);
+        byte idade = 20;
+        short rm = 456;
+        int ano = 2026;
+        long id = 123214;
+        float altura = 1.80f;
+        double preco = 340.00;
+
+
+        arro arro1 = new arro()
+    }
+}
---
+++ Apostila003/src/arro.java
@@ -0,0 +1,12 @@
+public class arro {
+
+    String modelo;
+    int velocidadeMaxima;
+    String cor;
+    double peso;
+    boolean automatico;
+    short ano;
+    float altura;
+
+
+}
