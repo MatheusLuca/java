21a7317a9ac2c7abf081661f574c4f33605e7f9b
matheusluca <luca_matheus@live.com>
2026-04-15 21:53:27 -0300

    Feat: criacao do objeto produto.

--- src/br/fiap/fiap/transportadora/view/Terminal.java
+++ src/br/fiap/fiap/transportadora/view/Terminal.java
@@ -1,7 +1,17 @@
 package br.fiap.fiap.transportadora.view;
 
+import br.fiap.fiap.transportadora.model.Produto;
+
 public class Terminal {
     public static void main(String[] args) {
+        Produto prod = new Produto();
+
+        prod.setId(1);
+        prod.setNome("Sargadim");
+        prod.setFragil(true);
+        prod.setPeso(0.250);
+        prod.setVolume(1);
+
 
     }
 }
