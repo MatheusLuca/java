29e3589cb0f4fcefb65975761144ea37ebb02cb7
matheusluca <luca_matheus@live.com>
2026-04-15 22:31:06 -0300

    Feat: Implementeção de dois objetos usando os construtores criados na classe produto.

--- src/br/fiap/fiap/transportadora/view/Terminal.java
+++ src/br/fiap/fiap/transportadora/view/Terminal.java
@@ -6,7 +6,7 @@ public class Terminal {
     public static void main(String[] args) {
         //Construtor entra no new quando criamos um objeto.
         Produto prod = new Produto(1, "Salgadim", true, 0.250, 1);
-
+        Produto prod1 = new Produto("Batatinha", 80, 2);
 
 
 
